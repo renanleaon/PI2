5ba8457ce2dc854be48ba7fb20b6a7277fd875b6
RenanLiao <renanleaon@gmail.com>
2024-10-27 17:15:08 -0300

    agora vai

--- setup/settings.py
+++ setup/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['164.152.34.246']
 
 
 # Application definition
