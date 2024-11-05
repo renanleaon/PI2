53edb5cbb3853fd1d8f22b475ec16a31c5c932b9
RenanLiao <renanleaon@gmail.com>
2024-11-05 03:45:20 -0300

    TESTE2

--- setup/settings.py
+++ setup/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['150.230.67.116', '127.0.0.1']
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
